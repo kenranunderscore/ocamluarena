048265851ca0a79350a213b4ed235613152450ec
Johannes Maier <johannes.maier@mailbox.org>
2024-03-10 21:25:31 +0100

    Hide Lua details by separating from player state

--- lib/main.ml
+++ lib/main.ml
@@ -3,6 +3,16 @@ let arena_width = 1000
 let arena_height = 800
 let player_diameter = 50.
 
+type player_command =
+  | Move of float
+  | Turn_right of float
+[@@deriving show]
+
+module type PLAYER = sig
+  val on_tick : int -> player_command list
+end
+
+(* TODO: rename *)
 module Player = struct
   type t =
     { name : string
@@ -15,119 +25,7 @@ module Player = struct
   let compare p1 p2 = compare p1.name p2.name
 end
 
-type position =
-  { x : float
-  ; y : float
-  }
-
-type player_command =
-  | Move of float
-  | Turn_right of float
-[@@deriving show]
-
-type player_intent =
-  { distance : float
-  ; angle : float
-  }
-
-let default_intent = { distance = 0.0; angle = 0.0 }
-
-type player_state =
-  { lua_state : Lua.state
-  ; pos : position
-  ; commands : player_command list
-  ; intent : player_intent
-  }
-
-module PlayerMap = Map.Make (Player)
-
-type game_state = { player_states : player_state ref PlayerMap.t }
-
-let lua_get_color ls =
-  Lua.getfield ls (-1) "color";
-  Lua.getfield ls (-1) "red";
-  let red = Lua.tointeger ls (-1) in
-  Lua.getfield ls (-2) "green";
-  let green = Lua.tointeger ls (-1) in
-  Lua.getfield ls (-3) "blue";
-  let blue = Lua.tointeger ls (-1) in
-  Lua.pop ls 4;
-  Color.make ~red ~green ~blue
-;;
-
-(* TODO: maybe add some unique identifier *)
-let lua_load_player path =
-  let ls = Lua.newstate () in
-  Lua.openlibs ls;
-  (* TODO: clean error handling *)
-  if Lua.dofile ls path
-  then (
-    Printf.printf "loading player from '%s'...\n%!" path;
-    Lua.getfield ls (-1) "meta";
-    Lua.getfield ls (-1) "name";
-    match Lua.tostring ls (-1) with
-    | None -> failwithf "player.meta.name missing\n%!"
-    | Some name ->
-      (* pop the 'name' *)
-      Lua.pop ls 1;
-      let color = lua_get_color ls in
-      (* pop the 'meta' table *)
-      Lua.pop ls 1;
-      let player = Player.make ~name ~color in
-      player, ls)
-  else failwithf "player could not be loaded: '%s'\n%!" path
-;;
-
-let create_lua_api player_state =
-  let ls = !player_state.lua_state in
-  Lua.pushmodule
-    ls
-    "me"
-    [ ( "x"
-      , fun l ->
-          Lua.pushnumber l !player_state.pos.x;
-          1 )
-    ; ( "y"
-      , fun l ->
-          Lua.pushnumber l !player_state.pos.y;
-          1 )
-    ; ( "move"
-      , fun l ->
-          let distance = Lua.tonumber l (-1) in
-          Lua.pop ls 1;
-          Lua.newuserdata ls (Move distance);
-          1 )
-    ]
-;;
-
-let load_player path =
-  let player, lua_state = lua_load_player ("players/" ^ path) in
-  let pos =
-    (* workaround *)
-    match path with
-    | "lloyd.lua" -> { x = 100.; y = 50. }
-    | "cole.lua" -> { x = 200.; y = 50. }
-    | _ -> { x = 0.; y = 0. }
-  in
-  let player_state = ref { pos; lua_state; commands = []; intent = default_intent } in
-  create_lua_api player_state;
-  player, player_state
-;;
-
-let draw_player renderer player player_state =
-  let x = player_state.pos.x -. (player_diameter /. 2.) |> Int.of_float in
-  let y = player_state.pos.y -. (player_diameter /. 2.) |> Int.of_float in
-  let rect = Sdl.Rect.create ~x ~y ~w:50 ~h:50 in
-  let color = Player.color player in
-  Sdl.set_render_draw_color renderer ~r:color.red ~g:color.green ~b:color.blue;
-  Sdl.render_fill_rect renderer rect
-;;
-
-(* TODO: implement: take the 'latest' (according to event order?) command of
-   each type *)
-let reduce_commands commands = commands
-
-let call_on_tick_event ls tick =
+let lua_call_on_tick_event ls tick =
   Lua.getfield ls 1 "on_tick";
   if Lua.isnil ls (-1)
   then (
@@ -149,6 +47,113 @@ let call_on_tick_event ls tick =
       | _ -> [])
     else [])
 ;;
+
+let make_lua_player ls : (module PLAYER) =
+  (module struct
+    let on_tick tick = lua_call_on_tick_event ls tick
+  end)
+;;
+
+type position =
+  { x : float
+  ; y : float
+  }
+
+type player_intent =
+  { distance : float
+  ; angle : float
+  }
+
+let default_intent = { distance = 0.0; angle = 0.0 }
+
+type player_state =
+  { pos : position
+  ; commands : player_command list
+  ; intent : player_intent
+  }
+
+let make_initial_state pos = { pos; commands = []; intent = default_intent }
+
+module PlayerMap = Map.Make (Player)
+
+(* TODO: add type for map values. player_data? *)
+type game_state = { players : (player_state ref * (module PLAYER)) PlayerMap.t }
+
+let lua_get_color ls =
+  Lua.getfield ls (-1) "color";
+  Lua.getfield ls (-1) "red";
+  let red = Lua.tointeger ls (-1) in
+  Lua.getfield ls (-2) "green";
+  let green = Lua.tointeger ls (-1) in
+  Lua.getfield ls (-3) "blue";
+  let blue = Lua.tointeger ls (-1) in
+  Lua.pop ls 4;
+  Color.make ~red ~green ~blue
+;;
+
+(* TODO: maybe add some unique identifier *)
+let lua_load_lua_player path =
+  let ls = Lua.newstate () in
+  Lua.openlibs ls;
+  (* TODO: clean error handling *)
+  if Lua.dofile ls path
+  then (
+    Printf.printf "loading player from '%s'...\n%!" path;
+    Lua.getfield ls (-1) "meta";
+    Lua.getfield ls (-1) "name";
+    match Lua.tostring ls (-1) with
+    | None -> failwithf "player.meta.name missing\n%!"
+    | Some name ->
+      (* pop the 'name' *)
+      Lua.pop ls 1;
+      let color = lua_get_color ls in
+      (* pop the 'meta' table *)
+      Lua.pop ls 1;
+      let player = Player.make ~name ~color in
+      player, ls)
+  else failwithf "player could not be loaded: '%s'\n%!" path
+;;
+
+let create_lua_api ls player_state =
+  Lua.pushmodule
+    ls
+    "me"
+    [ ( "x"
+      , fun l ->
+          Lua.pushnumber l !player_state.pos.x;
+          1 )
+    ; ( "y"
+      , fun l ->
+          Lua.pushnumber l !player_state.pos.y;
+          1 )
+    ; ( "move"
+      , fun l ->
+          let distance = Lua.tonumber l (-1) in
+          Lua.pop ls 1;
+          Lua.newuserdata ls (Move distance);
+          1 )
+    ]
+;;
+
+let load_lua_player path player_state =
+  let player, lua_state = lua_load_lua_player ("players/" ^ path) in
+  let p = make_lua_player lua_state in
+  create_lua_api lua_state player_state;
+  player, p
+;;
+
+let draw_player renderer player player_state =
+  let x = player_state.pos.x -. (player_diameter /. 2.) |> Int.of_float in
+  let y = player_state.pos.y -. (player_diameter /. 2.) |> Int.of_float in
+  let rect = Sdl.Rect.create ~x ~y ~w:50 ~h:50 in
+  let color = Player.color player in
+  Sdl.set_render_draw_color renderer ~r:color.red ~g:color.green ~b:color.blue;
+  Sdl.render_fill_rect renderer rect
+;;
+
+(* TODO: implement: take the 'latest' (according to event order?) command of
+   each type *)
+let reduce_commands commands = commands
 
 type movement_change =
   { remaining_intent : player_intent
@@ -187,8 +192,8 @@ let is_valid_position player ({ x; y } as p) game_state =
     && y +. r <= Float.of_int arena_height
   in
   let would_hit_other_player =
-    game_state.player_states
-    |> PlayerMap.exists (fun player' state ->
+    game_state.players
+    |> PlayerMap.exists (fun player' (state, _impl) ->
       player != player' && players_collide p !state.pos)
   in
   stays_inside_arena && not would_hit_other_player
@@ -228,12 +233,12 @@ let find_colliding_players positions =
 
 let run_tick game_state tick =
   let moving_players =
-    game_state.player_states
-    |> PlayerMap.filter_map (fun player state ->
+    game_state.players
+    |> PlayerMap.filter_map (fun player (state, impl) ->
       Printf.printf "  asking player: %s\n%!" player.name;
       let ps = !state in
-      let ls = ps.lua_state in
-      let tick_commands = call_on_tick_event ls tick in
+      let module M = (val impl : PLAYER) in
+      let tick_commands = M.on_tick tick in
       let commands = reduce_commands tick_commands in
       let new_intent = determine_intent ps.intent commands in
       (* TODO: always update intent? *)
@@ -274,16 +279,23 @@ let main_loop renderer game_state =
     ignore @@ run_tick game_state !tick;
     Sdl.set_render_draw_color renderer ~r:20 ~g:20 ~b:20;
     Sdl.render_clear renderer;
-    game_state.player_states
-    |> PlayerMap.iter (fun p state -> draw_player renderer p !state);
+    game_state.players
+    |> PlayerMap.iter (fun p (state, _impl) -> draw_player renderer p !state);
     Sdl.render_present renderer;
     Thread.delay 0.01
   done
 ;;
 
 let main () =
-  let players = [ load_player "lloyd.lua"; load_player "cole.lua" ] in
-  let game_state = { player_states = PlayerMap.of_list players } in
+  let lloyd_state = ref @@ make_initial_state { x = 100.; y = 50. } in
+  let lloyd_meta, lloyd_module = load_lua_player "lloyd.lua" lloyd_state in
+  let cole_state = ref @@ make_initial_state { x = 450.; y = 80. } in
+  let cole_meta, cole_module = load_lua_player "cole.lua" cole_state in
+  let players =
+    PlayerMap.of_list
+      [ lloyd_meta, (lloyd_state, lloyd_module); cole_meta, (cole_state, cole_module) ]
+  in
+  let game_state = { players } in
   Sdl.with_sdl (fun () ->
     Sdl.with_window_and_renderer
       ~w:arena_width
